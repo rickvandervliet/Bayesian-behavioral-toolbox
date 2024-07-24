046ecbc06387067dd6942381d6121c705089c5ea
rickvandervliet <38972062+rickvandervliet@users.noreply.github.com>
2024-07-24 12:31:23 +0200

    Update One-rate-state-space-model.py

--- One-rate-state-space-model.py
+++ One-rate-state-space-model.py
@@ -8,7 +8,7 @@ from os.path import dirname, realpath, join as pjoin
 def main():
     # Get data
     dir_path = dirname(realpath(__file__))
-    mat_fname = pjoin(dir_path, 'dataForBayesianAnalysisEEG-VM.mat')
+    mat_fname = pjoin(dir_path, 'Behavioral-data/dataForBayesianAnalysisEEG-VM.mat')
     posterior_fname = 'posteriorDatav2.nc'
     n_steps = 900
     n_subjects = 60
@@ -27,17 +27,17 @@ def main():
         A1std = pm.Gamma('A1std', mu=0.5, sigma=0.25)
         B1mu = pm.Normal('B1mu', mu=-2, sigma=0.25)
         B1std = pm.Gamma('B1std', mu=0.5, sigma=0.25)
-        #etamu = pm.Gamma('etamu', mu=0.5, sigma=0.25)
-        #etastd = pm.Gamma('etastd', mu=0.5, sigma=0.25)
-        #epsilonmu = pm.Gamma('epsilonmu', mu=3, sigma=0.25)
-        #epsilonstd = pm.Gamma('epsilonstd', mu=0.5, sigma=0.25)
+        etamu = pm.Gamma('etamu', mu=0.5, sigma=0.25)
+        etastd = pm.Gamma('etastd', mu=0.5, sigma=0.25)
+        epsilonmu = pm.Gamma('epsilonmu', mu=3, sigma=0.25)
+        epsilonstd = pm.Gamma('epsilonstd', mu=0.5, sigma=0.25)
 
         A1 = pm.Normal('A1', mu=A1mu, sigma=A1std, dims='subjects')
         B1 = pm.Normal('B1', mu=B1mu, sigma=B1std, dims='subjects')
         A = pm.Deterministic('A', pm.math.invlogit(A1))
         B = pm.Deterministic('B', pm.math.invlogit(B1))
-        sigma_eta = pm.Gamma('sigma_eta', mu=0.5, sigma=0.25, dims='subjects')
-        sigma_epsilon = pm.Gamma('sigma_epsilon', mu=3, sigma=0.5, dims='subjects')
+        sigma_eta = pm.Gamma('sigma_eta', mu=etamu, sigma=etastd, dims='subjects')
+        sigma_epsilon = pm.Gamma('sigma_epsilon', mu=epsilonmu, sigma=epsilonstd, dims='subjects')
 
         x_init = pm.Normal('x_init', mu=0, sigma=sigma_eta)
         eta = pm.Normal('eta', mu=0, sigma=sigma_eta, dims=['time', 'subjects'])
@@ -56,7 +56,7 @@ def main():
         
         x = pt.concatenate([[x_init], x[:-1,]], axis=0)
         y_hat = pm.Normal('y_hat',mu=x,sigma=sigma_epsilon,observed=data_y,dims=['time', 'subjects'])
-        idata = pm.sample(cores=4,chains=4,draws=1000,tune=1000,init='adapt_diag')
+        idata = pm.sample(cores=8,chains=4,draws=10000,tune=10000,init='adapt_diag')
         idata.to_netcdf(posterior_fname)
 
 if __name__ == '__main__':
